195de6e370e9cbb24498d24334b2e96d307545d6
iShi0n <guibs225@gmail.com>
2021-01-17 21:42:49 -0300

    adicionado banner de ajuda

--- downloader.py
+++ downloader.py
@@ -2,8 +2,21 @@ from sys import argv
 from youtube import YouTube
 from soundcloud import SoundCloud
 
+
+#TODO: melhorar banner
+usage = f"""usage: python3 {argv[0]} PLAYLIST [pattern]
+
+Download albums from youtube/soundcloud and set metadata in mp3 files
+
+positional arguments:
+    playlist: youtube/soundcloud playlist url
+    pattern: (optional) pattern to be removed from the track title
+
+eg.: python3 {argv[0]} https://www.youtube.com/playlist?list=PLGeJR8ZOrTZdMuBWM9IYta6IoHKku0nH4 'bladee - '
+"""
+
 if len(argv) < 2:
-    exit("error: invalid argvs")
+    exit("error: invalid argvs\n"+usage)
 
 album = argv[1]
 remove_from_title = ""

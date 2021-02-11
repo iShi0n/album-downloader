3642207f138029209a4a49a36d123f6751b699bc
iShi0n <guibs225@gmail.com>
2021-02-11 10:03:02 -0300

    libs importadas

--- lyrics.py
+++ lyrics.py
@@ -1,10 +1,19 @@
+from __future__ import annotations
+import json
+import re
+
+import requests
+
+
 class Genius:
     def __init__(self) -> None:
         pass
-    
+
     # (?<=\.parse\(\').*}}}}(?='\);) REGEX PEGA JSON COM LETRA
     # dar replace('\\', '')
     # dar json.dumps
     # dar json.loads
     # lyricsdata
-    
+
+    def search(self, name: str):
+        ...

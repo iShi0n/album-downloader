b6d55cf811935194e16093988e819b5769e5e9ed
iShi0n <guibs225@gmail.com>
2021-02-11 10:01:04 -0300

    add: lyrics.py

---
+++ lyrics.py
@@ -0,0 +1,10 @@
+class Genius:
+    def __init__(self) -> None:
+        pass
+    
+    # (?<=\.parse\(\').*}}}}(?='\);) REGEX PEGA JSON COM LETRA
+    # dar replace('\\', '')
+    # dar json.dumps
+    # dar json.loads
+    # lyricsdata
+    
